c4c59bb33b18b3f650da7e119869e42b3e9b1039
Canis Ignem <jonperezetxebarria@gmail.com>
2021-08-23 18:22:13 +0200

    finishing touches

--- 5. Deep Learning/MLP Regression/Stock_sol/model.py
+++ 5. Deep Learning/MLP Regression/Stock_sol/model.py
@@ -9,24 +9,25 @@ class MLP(nn.Module):
             self.layers = [ nn.Linear(input_size, output_size ) ]
         else:
 
-            self.layers = [self.fc1]
+            self.layers = [nn.Linear(input_size, hidden_sizes[0] )]
 
-            for i in range(1,hidden_sizes):
+            for i in range(1,len(hidden_sizes)):
                 self.layers.append(nn.Linear(hidden_sizes[i-1], hidden_sizes[i]))
             
             self.layers.append( nn.Linear(hidden_sizes[-1], output_size ))
-
+            
+            self.layers = nn.ModuleList(self.layers)
 
     def forward(self, x):
 
-        for i in range(len(self.layers-1)):
+        for i in range(len(self.layers)-1):
             x = self.layers[i](x)
-            x = nn.ReLU(x)
+            x = nn.functional.relu(x)
         x = self.layers[-1](x)
         return x
 
         
 
-model = MLP(10,1, hidden_sizes=[7,4])
+model = MLP(8,1, hidden_sizes=[7,4])
 
-print( summary(model,(10,8)) )
+print( summary(model,(8,)) )
